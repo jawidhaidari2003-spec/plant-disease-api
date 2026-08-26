73c1b4bc19073dd1a9b31c699a4c5afa7903f5b7
jawidhaidari2003-spec <jawidhaidari2003@gmail.com>
2026-08-27 01:17:21 +0430

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import numpy as np
 import tensorflow as tf
 from io import BytesIO
 from PIL import Image
-import os  # برای خواندن پورت سرور اضافه شد
 
-# آدرس ویندوز حذف شد و آدرس نسبی فایل مدل که در گیت‌هاب قرار دارد جایگزین شد
-MODEL = tf.keras.models.load_model("3.keras")
+# لود کردن مدل فوق‌سبک TFLite (این دستور رم سرور را پر نمی‌کند)
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
-
 app = FastAPI()
 
 @app.get("/ping")
@@ -18,16 +20,23 @@ async def ping():
     return "Hello I am Rohullah"
 
 def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    return image
+    # تغییر اندازه عکس به سایز استاندارد ۲۵۶ در ۲۵۶ که برای مدل‌های سی‌ان‌ان رایج است
+    image = Image.open(BytesIO(data)).resize((256, 256))
+    img_array = np.array(image, dtype=np.float32)
+    return img_array
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-    pred = MODEL.predict(img_batch)
-    predicted_class = CLASS_NAMES[np.argmax(pred[0])]
-    confidence = np.max(pred[0])  
+    
+    # اجرای مدل لایت
+    interpreter.set_tensor(input_details['index'], img_batch)
+    interpreter.invoke()
+    pred = interpreter.get_tensor(output_details['index'])
+    
+    predicted_class = CLASS_NAMES[np.argmax(pred)]
+    confidence = np.max(pred)  
     
     return {
         "Class": predicted_class, 
